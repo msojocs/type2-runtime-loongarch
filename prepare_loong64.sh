802d6af245df4844dbdc1b77bdefc61518bc43ea
msojocs <jiyecafe@gmail.com>
2024-05-20 15:35:58 +0800

    fix: wget

--- prepare_loong64.sh
+++ prepare_loong64.sh
@@ -10,5 +10,5 @@ trap cleanup EXIT
 mkdir -p tmp
 cd tmp
 # from https://packages.ubuntu.com/noble/amd64/qemu-user-static/download
-wget -c wget -c http://kr.archive.ubuntu.com/ubuntu/pool/universe/q/qemu/qemu-user-static_8.2.2+ds-0ubuntu1_amd64.deb
+wget -c http://kr.archive.ubuntu.com/ubuntu/pool/universe/q/qemu/qemu-user-static_8.2.2+ds-0ubuntu1_amd64.deb
 sudo dpkg -i qemu-user-static_8.2.2+ds-0ubuntu1_amd64.deb
